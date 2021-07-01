34312057ea5365467ce64f2d44c2455f47441707
deegidev <62577512+deegidev@users.noreply.github.com>
2021-07-01 17:50:16 -0400

    Add files via upload

---
+++ Newbie Programming Vocabulary/app.js
@@ -0,0 +1,8 @@
+function textToggle() {
+ var text = document.getElementById("explain"); 
+if (text.style.display === "none") {
+    text.style.display = "block";
+  } else {
+    text.style.display = "none";
+  }
+}
