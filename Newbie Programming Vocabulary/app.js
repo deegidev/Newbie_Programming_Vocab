43a77c52e1adcf5e9fbdfba5616a302b0916b0ab
deegidev <62577512+deegidev@users.noreply.github.com>
2021-07-07 14:09:23 -0400

    Delete Newbie Programming Vocabulary directory

--- Newbie Programming Vocabulary/app.js
+++
@@ -1,8 +0,0 @@
-function textToggle() {
- var text = document.getElementById("explain"); 
-if (text.style.display === "none") {
-    text.style.display = "block";
-  } else {
-    text.style.display = "none";
-  }
-}
